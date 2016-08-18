994ffad3c679628e8dc508a3e8662e211f028dfb
Lexi Brent <lexi@lexibrent.com>
2016-08-18 18:23:36 +1000

    Add comments to opcodes.py

--- opcodes.py
+++ opcodes.py
@@ -1,17 +1,18 @@
-# String -> Constant mapping of disasm opcodes
+# Map disasm opcode strings to Python constants
 JUMP = "JUMP"
 JUMPI = "JUMPI"
 RETURN = "RETURN"
 SUICIDE = "SUICIDE"
 STOP = "STOP"
-
-PUSH_PREFIX = "PUSH"
-
 JUMPDEST = "JUMPDEST"
 
-def alters_flow(opcode):
+# Common prefix of "PUSH"-style operations, used to
+# check if a given operation is a PUSH
+PUSH_PREFIX = "PUSH"
+
+def alters_flow(opcode:str):
   """
-  Returns True if opcode alters the EVM control flow.
+  Returns True if the given opcode (string) alters EVM control flow.
   """
   return opcode in [
     JUMP, JUMPI, RETURN,
