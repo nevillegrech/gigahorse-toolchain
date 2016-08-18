cc9df661f45ee3c4eb731079320214c8eee63a25
Lexi Brent <lexi@lexibrent.com>
2016-08-18 18:20:14 +1000

    Add docstrings and comments to dis2bb

--- dis2bb.py
+++ dis2bb.py
@@ -3,77 +3,124 @@
 import fileinput
 from opcodes import *
 
+# Separator to be used for string representation of blocks
 BLOCK_SEP = "\n---"
 
 class BasicBlock:
-  def __init__(self, start=None, end=None):
+  """
+  Represents a single basic block in the control flow graph (CFG), including
+  its parent and child nodes in the graph structure.
+  """
+  def __init__(self, start:int=None, end:int=None):
+    """Creates a new basic block containing disassembly lines between the
+    specified start index and the specified end index (inclusive)."""
     self.start = start
     self.end = end
+    # DisasmLine objects contained in this block's range
     self.lines = []
+    # BasicBlock objects which pass control to this block
     self.parents = []
+    # BasicBlock objects which this block passes control to
     self.children = []
 
   def __len__(self):
+    """Returns the number of DisasmLines contained within this block."""
     return self.end - self.start
 
   def __str__(self):
+    """Returns a string representation of this block and all lines in it."""
+    # str(BasicBlock) is as a newline-separated list of str(DisasmLine)
+    # with the BLOCK_SEP appended after the last line
     return "\n".join(str(l) for l in self.lines) + BLOCK_SEP
 
-  def split(self, start):
+  def split(self, start:int):
     """Splits this block into a new block, starting at the specified
     start line number. Returns the new BasicBlock."""
+    # Create the new block and assign the line ranges
     new_block = BasicBlock(start, self.end)
     self.end = start - 1
+    # Split the disasm lines
     new_block.lines = self.lines[start:]
     self.lines = self.lines[:start]
+    # Update the block pointer in each line object
     self.update_lines()
     new_block.update_lines()
     return new_block
 
   def update_lines(self):
-    """Updates the pointer in each line to point to this block. This is called
-    by BasicBlock.split()."""
+    """Updates the pointer in each DisasmLine object to point to this block.
+    This is called by BasicBlock.split() after a split to correct any
+    references to the original (pre-split) block."""
     for l in self.lines:
       l.block = self
 
 class DisasmLine:
-  def __init__(self, pc, opcode, value=None):
+  """Represents a single line of EVM bytecode disassembly as produced by the
+  official Ethereum 'disasm' disassembler."""
+  def __init__(self, pc:str, opcode:str, value:str=None):
+    """Create a new DisasmLine object from the given strings which should
+    correspond to disasm output. Each line of disasm output is structured as
+    follows:
+              PC <spaces> OPCODE <spaces> => CONSTANT
+    where:
+            PC is the program counter (base 10 integer)
+        OPCODE is a textual representation of an EVM instruction code
+      CONSTANT is a hexadecimal value with 0x notational prefix
+      <spaces> is a variable number of spaces
+    For instructions with no hard-coded constant data (i.e. non-PUSH
+    instructions), the disasm output only includes PC and OPCODE; i.e.
+              PC <spaces> OPCODE
+
+    If None is passed to the value parameter, the instruction is assumed to
+    contain no CONSTANT (as in the second example above).
+    """
+    # Program counter, stored as a base-10 int
     self.pc = int(pc)
+    # VM operation code, stored as a string
     self.opcode = opcode
+    # Constant value, stored as a base-10 int or None
     self.value = int(value, 16) if value != None else value
+    # BasicBlock object to which this line belongs
     self.block = None
 
   @staticmethod
-  def from_raw(line):
+  def from_raw(line:str):
+    """Creates and returns a new BasicBlock object from a raw line of
+    disassembly. The line should be from Ethereum's disasm disassembler."""
     l = line.split()
     if len(l) > 3:
       return DisasmLine(l[0], l[1], l[3])
     elif len(l) > 1:
       return DisasmLine(l[0], l[1])
     else:
-      raise Exception("Invalid disassembly line: " + str(l))
+      raise Exception("Could not parse invalid disassembly format: " + str(l))
 
   def __str__(self):
     if self.value is None:
-      return "{0} {1}".format(self.pc, self.opcode)
+      return "{0} {1}".format(hex(self.pc), self.opcode)
     else:
-      return "{0} {1} {2}".format(self.pc, self.opcode, self.value)
+      return "{0} {1} {2}".format(hex(self.pc), self.opcode, hex(self.value))
 
   def __repr__(self):
     return "<{0} object {1}: {2}>".format(
       self.__class__.__name__,
       hex(id(self)),
-      self.__str__())
+      self.__str__()
+    )
 
 
-def listdict_add(ldict, key, val):
-  """Adds a new value to the list in a given list-valued
-  dictionary for the given key"""
+def listdict_add(ldict:dict, key:object, val:object):
+  """Adds a given value (val) to the list in a given list-valued dictionary
+  (ldict) for the given key"""
   if key in ldict:
     ldict[key].append(val)
   else:
     ldict[key] = [val]
 
+# Construct a list of DisasmLine objects from the raw input disassembly lines,
+# ignoring the first line of input (which is the bytecode's hex representation
+# when using Ethereum's disasm tool). Any line which does not produce enough
+# tokens to be valid disassembly after being split() is also ignored.
 lines = [
   DisasmLine.from_raw(l)
   for i, l in enumerate(fileinput.input())
@@ -81,17 +128,20 @@ lines = [
 ]
 
 # Mapping of base 10 program counters to line indices
+# (Used for mapping jump destinations to actual disassembly lines)
 pc2line = {l.pc: i for i, l in enumerate(lines)}
 
-# Mapping of potential leaders (PCs) based on JUMP destinations discovered by
-# peephole analysis; in the form: dest PC => [from blocks]
+# Mapping of potential leaders (stored as base-10 PC values) based on JUMP
+# destinations discovered by peephole analysis; in the form:
+#   PC => list(BasicBlocks)
 potential_leaders = dict()
 
-# List of DisasmLines which represent JUMPs with an unresolved
-# destination address
+# List of DisasmLines which represent JUMPs with an unresolved destination address
+# Either these jumps use computed addresses, or the destination is pushed to
+# the stack in a previous block.
 unresolved_jumps = []
 
-# List of confirmed blocks in the tuple format described below
+# List of BasicBlock objects making up our CFG
 blocks = []
 
 # block currently being processed
