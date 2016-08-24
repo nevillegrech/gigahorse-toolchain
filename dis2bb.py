72b02fa5c0d654ff6443534321d058302a56cab4
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-08-24 12:53:38 +1000

    Fixed-point analysis of stack sizes added.

--- dis2bb.py
+++ dis2bb.py
@@ -6,4 +6,16 @@ import fileinput
 # Local project imports
 from cfglib import *
 
+from stacksizeanalysis import *
+
 cfg = ControlFlowGraph(fileinput.input())
+
+
+entry, exit = run_analysis(cfg)
+
+for block in cfg.blocks:
+	print("----")
+	print(entry[block])
+	print(block)
+	print(block_stack_delta(block))
+	print(exit[block])
--- stacksizeanalysis.py
+++ stacksizeanalysis.py
@@ -1,4 +1,5 @@
-from cfglib import BasicBlock
+from cfglib import *
+from lattice import *
 
 def block_stack_delta(block:BasicBlock):
 	delta = 0
@@ -6,4 +7,36 @@ def block_stack_delta(block:BasicBlock):
 	for line in block.lines:
 		delta += line.opcode.stack_delta()
 
-	return stack_delta
+	return delta
+
+def run_analysis(cfg:ControlFlowGraph):
+	# Stack size information per block at entry and exit points.
+	entry_info = {block: TOP for block in cfg.blocks}
+	exit_info = {block: TOP for block in cfg.blocks}
+	block_deltas = {block: IntLatticeElement(block_stack_delta(block)) for block in cfg.blocks}
+
+	# Add a distinguished start block which does nothing.
+	start_block = BasicBlock()
+	cfg.root.parents.append(start_block)
+	exit_info[start_block] = IntLatticeElement(0)
+
+	# Find the fixed point that is the meet-over-paths solution
+	queue = list(cfg.blocks)
+
+	while queue:
+		current = queue.pop()
+
+		# Calculate the new entry value for the current block.
+		new_entry = meet_all([exit_info[parent] for parent in current.parents])
+
+		# If the entry value changed, we have to recompute
+		# its exit value, and the entry value for its children, eventually.
+		if new_entry != entry_info[current]:
+			entry_info[current] = new_entry
+			exit_info[current] = new_entry + block_deltas[current]
+			queue += current.children
+
+	# Remove the start block that was added.
+	cfg.root.parents.pop()
+
+	return (entry_info, exit_info)
