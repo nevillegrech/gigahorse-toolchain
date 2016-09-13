8bec8724aae2f19a0eaefd3fa4ae0cbe63d071a9
Lexi Brent <lexi@lexibrent.com>
2016-09-14 01:41:26 +1000

    Add exporter ABC and PrintExporter example

--- src/exporter.py
+++ src/exporter.py
@@ -17,3 +17,24 @@ class CFGExporter(patterns.Visitor):
     Args:
       cfg_node: the CFG node to be visited.
     """
+
+  @abc.abstractmethod
+  def export(self):
+    """
+    Abstract method which performs the final export using state collected
+    during visitations.
+    """
+
+class PrintExporter(CFGExporter):
+  def __init__(self, ordered=True):
+    self.ordered = ordered
+    self.nodes = []
+
+  def visit(self, node):
+    self.nodes.append((node.entry, str(node)))
+
+  def export(self):
+    if self.ordered:
+      self.nodes.sort(key=lambda n: n[0])
+    for n in self.nodes:
+      print(n[1])
