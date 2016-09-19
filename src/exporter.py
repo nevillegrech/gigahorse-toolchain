ab703bb5e2b7a2613be870bdc36b738f864df7a7
Lexi Brent <lexi@lexibrent.com>
2016-09-19 13:01:48 +1000

    Modify Exporter ABC definition, modify CFGPrintExporter to use new DynamicVisitor pattern

--- src/exporter.py
+++ src/exporter.py
@@ -3,25 +3,36 @@
 import abc
 
 import cfg
-import tac_cfg
-import patterns
 import opcodes
+import patterns
+import tac_cfg
 
 class Exporter(abc.ABC):
+  def __init__(self, source:object):
+    """
+    Args:
+      source: object instance to be exported
+    """
+    self.source = source
+
   @abc.abstractmethod
   def export(self):
     """
-    Abstract method which performs the final export using state collected
-    during visitations.
+    Exports the source object to an implementation-specific format.
     """
 
 
-class CFGPrintExporter(Exporter, patterns.Visitor):
-  def __init__(self, ordered=True):
+class CFGPrintExporter(Exporter, patterns.DynamicVisitor):
+  def __init__(self, source:object, ordered:bool=True):
+    super().__init__(source)
     self.ordered = ordered
     self.nodes = []
+    self.source.accept(self)
 
-  def visit(self, node):
+  def visit_ControlFlowGraph(self, cfg):
+    pass
+
+  def visit_BasicBlock(self, node):
     self.nodes.append((node.entry, str(node)))
 
   def export(self):
@@ -32,23 +43,26 @@ class CFGPrintExporter(Exporter, patterns.Visitor):
 
 
 class CFGDotExporter(Exporter):
-  def export(self, cfg:tac_cfg.TACGraph, out_filename:str="cfg.dot"):
+  def __init__(self, cfg:cfg.ControlFlowGraph):
+    self.cfg = cfg
+
+  def export(self, out_filename:str="cfg.dot"):
     """From a CFG, generate a dotfile for drawing a pretty picture of it."""
     import networkx as nx
     from networkx.drawing.nx_pydot import write_dot
     G = nx.DiGraph()
-    G.add_nodes_from(b.ident() for b in cfg.blocks)
-    G.add_edges_from((p, s) for p, s in cfg.edge_list())
-    G.add_edges_from((block.ident(), "?") for block in cfg.blocks \
+    G.add_nodes_from(b.ident() for b in self.cfg.blocks)
+    G.add_edges_from((p, s) for p, s in self.cfg.edge_list())
+    G.add_edges_from((block.ident(), "?") for block in self.cfg.blocks \
                      if block.has_unresolved_jump)
 
-    returns = {block.ident(): "green" for block in cfg.blocks \
+    returns = {block.ident(): "green" for block in self.cfg.blocks \
                if block.tac_ops[-1].opcode == opcodes.RETURN}
-    stops = {block.ident(): "blue" for block in cfg.blocks \
+    stops = {block.ident(): "blue" for block in self.cfg.blocks \
              if block.tac_ops[-1].opcode == opcodes.STOP}
-    throws = {block.ident(): "red" for block in cfg.blocks \
+    throws = {block.ident(): "red" for block in self.cfg.blocks \
              if block.tac_ops[-1].opcode in [opcodes.THROW, opcodes.THROWI]}
-    suicides = {block.ident(): "purple" for block in cfg.blocks \
+    suicides = {block.ident(): "purple" for block in self.cfg.blocks \
                 if block.tac_ops[-1].opcode == opcodes.SUICIDE}
     color_dict = {**returns, **stops, **throws, **suicides}
     nx.set_node_attributes(G, "color", color_dict)
