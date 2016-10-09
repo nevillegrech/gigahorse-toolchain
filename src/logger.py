c662f67c284c528bdea84b5403fe7deb2ee56f4c
Lexi Brent <lexi@lexibrent.com>
2016-10-09 21:55:46 +1100

    More argparse features

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 """logger.py: global debug output logging"""
 
 import sys
+import typing
 
 LOG_LEVEL = 0
 """
@@ -19,9 +20,39 @@ class Verbosity:
   LOW = 1
   QUIET = 0
 
-def log(message:str, threshold:int = Verbosity.LOW):
+def log(message:str, threshold:int = Verbosity.LOW) -> None:
   """
   Log the given message to LOG_FILE if LOG_LEVEL exceeds threshold.
   """
-  if LOG_LEVEL >= threshold:
+  if threshold <= LOG_LEVEL:
     print(message, file=LOG_FILE)
+
+def log_high(message:str, *args:typing.Tuple[str]) -> None:
+  """
+  Log the given message at the HIGH Verbosity level.
+
+  Args:
+    message: message to be logged, can be a str.format compatible
+    *args: arguments for str.format substitutions in message
+  """
+  log(message.format(*args), Verbosity.HIGH)
+
+def log_med(message:str, *args:typing.Tuple[str]) -> None:
+  """
+  Log the given message at the MEDIUM Verbosity level.
+
+  Args:
+    message: message to be logged, can be a str.format compatible
+    *args: arguments for str.format substitutions in message
+  """
+  log(message.format(*args), Verbosity.MEDIUM)
+
+def log_low(message:str, *args:typing.Tuple[str]) -> None:
+  """
+  Log the given message at the LOW Verbosity level.
+
+  Args:
+    message: message to be logged, can be a str.format compatible
+    *args: arguments for str.format substitutions in message
+  """
+  log(message.format(*args), Verbosity.LOW)
