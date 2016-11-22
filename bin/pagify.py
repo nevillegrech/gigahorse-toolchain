39d18adb92a129c11595e09653844a328513df19
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-11-23 10:51:19 +1100

    Improve pagified CFG rendering slightly.

--- bin/pagify.py
+++ bin/pagify.py
@@ -4,6 +4,11 @@ filename = sys.argv[1]
 
 with open(filename + ".html", 'w') as page:
     page.write("<html><body>\n")
+    page.write("<style>\n")
+    page.write(".node\n{\n  transition: all 0.05s ease-out;\n}\n")
+    page.write(".node:hover\n{\n  stroke-width: 1.5;\n  cursor:pointer\n}\n")
+    page.write(".node:hover\nellipse\n{\n  fill: #EEE;\n}\n")
+    page.write("</style>")
 
     with open(filename, 'r') as svg:
         for line in svg.readlines()[3:]:
@@ -12,7 +17,7 @@ with open(filename + ".html", 'w') as page:
     page.write("""<textarea id="infobox" disabled=true rows=40 cols=100>\n""")
     page.write("""</textarea>\n""")
     #page.write("""<script src="pagify.js"></script>\n""")
-    page.write("""<script>\n""") 
+    page.write("""<script>\n""")
 
     with open("pagify.js", 'r') as script:
         for line in script.readlines():
