c4e26465a80b0ce38e03c893d4c0d5692ddfd02f
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-11-28 15:22:13 +1100

    Path fission does not split pre-split paths in a single cycle.

--- src/exporter.py
+++ src/exporter.py
@@ -267,8 +267,10 @@ class CFGDotExporter(Exporter):
     nx.set_node_attributes(G, "color", color_dict)
     nx.set_node_attributes(G, "fillcolor", "white")
     nx.set_node_attributes(G, "style", "filled")
-    nx.set_node_attributes(G, "id", {block.ident(): block.ident() for block in cfg.blocks})
-    nx.set_node_attributes(G, "tooltip", {block.ident(): str(block) for block in cfg.blocks})
+    nx.set_node_attributes(G, "id", {block.ident(): block.ident()
+                                     for block in cfg.blocks})
+    nx.set_node_attributes(G, "tooltip", {block.ident(): str(block)
+                                          for block in cfg.blocks})
 
     # Write non-dot files using pydot and Graphviz
     if "." in out_filename and not out_filename.endswith(".dot"):
