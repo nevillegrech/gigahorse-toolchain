577ad6f939f82265174d0bf04634abd434c322a4
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-01-09 16:17:13 +1100

    PR continued: pagifier and memtype documentation.

--- src/pagifier/pagify.py
+++ src/pagifier/pagify.py
@@ -1,4 +1,8 @@
-"""pagify.py: takes a CFG SVG and turns it into an interactive web page."""
+"""
+pagify.py: takes a CFG SVG and turns it into an interactive web page.
+
+Tightly coupled with pagify.js. In particular, the infobox id has to match up.
+"""
 import sys
 import os
 
@@ -11,36 +15,59 @@ def pagify(filename:str, out_filename:str=None) -> None:
       out_filename: the location to write the html file. By default,
                     the page is written to filename + ".html"
   """
+  #
   oname = filename + ".html" if out_filename is None else out_filename
   with open(oname, 'w') as page:
-    page.write("<html><body>\n"
-               "<style>\n"
-               ".node\n{\n  transition: all 0.05s ease-out;\n}\n"
-               ".node:hover\n{\n  stroke-width: 1.5;\n  cursor:pointer\n}\n"
-               ".node:hover\nellipse\n{\n  fill: #EEE;\n}\n"
-               "textarea#infobox {\n"
-               "  position: fixed;\n"
-               "  display: block;\n"
-               "  top: 0;\n"
-               "  right: 0;\n}\n"
-               "</style>")
+    page.write("""
+               <html>
+               <body>
+               <style>
+               .node
+               {
+                 transition: all 0.05s ease-out;
+               }
+               .node:hover
+               {
+                 stroke-width: 1.5;
+                 cursor:pointer
+               }
+               .node:hover
+               ellipse
+               {
+                 fill: #EEE;
+               }
+               textarea#infobox {
+                 position: fixed;
+                 display: block;
+                 top: 0;
+                 right: 0;
+               }
+               </style>
+               """)
 
     with open(filename, 'r') as svg:
       for line in svg.readlines()[3:]:
         page.write(line)
 
-    page.write("""<textarea id="infobox" disabled=true rows=40 cols=100>\n""")
-    page.write("""</textarea>\n""")
-    # page.write("""<script src="pagify.js"></script>\n""")
-    page.write("""<script>\n""")
+    page.write("""
+               <textarea id="infobox" disabled=true rows=40 cols=100>
+               </textarea>
+               """)
 
+    page.write("""<script>\n""")
     folder = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(folder, "pagify.js"), 'r') as script:
       for line in script.readlines():
         page.write(" " + line)
 
-    page.write("""</script>\n""")
-    page.write("<html><body>\n")
+    page.write("""
+               </script>
+               </html>
+               </body>
+               """)
 
 if __name__ == "__main__":
-  pagify(sys.argv[1])
+  if len(sys.argv) < 2:
+    print("Please provide an input svg to pagify.")
+  else:
+    pagify(sys.argv[1])
