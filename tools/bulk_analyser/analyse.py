434a60504dc39815e61f0bccb6379fad3ca9db9b
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-01-10 21:20:20 +1100

    Generic dataflow fixed-point analysis function, cleaned up decompiler and bulk analyser with it, and other analyse.py improvements.

--- tools/bulk_analyser/analyse.py
+++ tools/bulk_analyser/analyse.py
@@ -2,6 +2,7 @@
 
 from os import listdir, makedirs
 from os.path import abspath, dirname, join
+import argparse
 from multiprocessing import Process, SimpleQueue, Manager, Event
 import time
 import sys
@@ -17,61 +18,147 @@ import tac_cfg
 import logger
 ll = logger.log_low
 
-### Constants ###
-unresolved = 0
-resolved = 1
-timeout = 3
-error = 4
 
-# The location to grab contracts from (as bytecode files)
-contract_dir = '../../../contract_dump/contracts'
-# The location to write the results
-results_dir = "non_error_results/"
+# Indices for different contract analysis categories.
+UNRESOLVED = 0
+RESOLVED = 1
+TIMEOUT = 3
+ERROR = 4
 
-# grab filenames from a specified file instead of a directory listing
-from_file = True
-filenames_file = "results/error.txt"
+DEFAULT_CONTRACT_DIR = '../../../contract_dump/contracts'
+"""Directory to fetch contract files from by default."""
 
-# If not process all, then a quantity of contracts specified by the indices
-process_all = True
-start_index = 0
-stop_index = 100
+DEFAULT_RESULTS_DIR = 'results/'
+"""Directory to write results to by default."""
 
-# Kill the analysis after this many seconds
-timeout_secs = 120
+DEFAULT_TIMEOUT = 120
+"""Default time before killing analysis of a contract."""
 
-# Wait a little to flush the files and join the processes when concluding
-flush_period = 3
+DEFAULT_MAX_ITER = -1
+"""Default max analysis iteration count."""
 
-# The number of times to run the analysis loop.
-analysis_iterations = 3
+DEFAULT_BAILOUT = -1
+"""Default analysis bailout time in seconds."""
+
+FLUSH_PERIOD = 3
+"""Wait a little to flush the files and join the processes when concluding."""
+
+
+parser = argparse.ArgumentParser(
+  description="A batch analyser for EVM bytecode programs.")
+
+parser.add_argument("-c",
+                    "--contract_dir",
+                    nargs="?",
+                    default=DEFAULT_CONTRACT_DIR,
+                    const=DEFAULT_CONTRACT_DIR,
+                    metavar="DIR",
+                    help="the location to grab contracts from (as bytecode "
+                         "files).")
+
+parser.add_argument("-r",
+                    "--results_dir",
+                    nargs="?",
+                    default=DEFAULT_RESULTS_DIR,
+                    const=DEFAULT_RESULTS_DIR,
+                    metavar="DIR",
+                    help="the location to write the results.")
+
+parser.add_argument("-f",
+                    "--from_file",
+                    nargs="?",
+                    default=None,
+                    metavar="FILE",
+                    help="A file to extract the filenames of the contracts "
+                         "to analyse from, rather than simply processing all "
+                         "files in the contracts directory.")
+
+parser.add_argument("-n",
+                    "--num_contracts",
+                    type=int,
+                    nargs="?",
+                    default=None,
+                    metavar="NUM",
+                    help="The maximum number of contracts to process in this "
+                         "batch. Unlimited by default.")
+
+parser.add_argument("-k",
+                    "--skip",
+                    type=int,
+                    nargs="?",
+                    default=0,
+                    const=0,
+                    metavar="NUM",
+                    help="Skip the the analysis of the first NUM contracts.")
+
+parser.add_argument("-t",
+                    "--timeout_secs",
+                    type=int,
+                    nargs="?",
+                    default=DEFAULT_TIMEOUT,
+                    const=DEFAULT_TIMEOUT,
+                    metavar="SECONDS",
+                    help="Forcibly halt analysing any single contact after "
+                         "the specified number of seconds.")
+
+parser.add_argument("-I",
+                    "--max_iter",
+                    type=int,
+                    nargs="?",
+                    default=DEFAULT_MAX_ITER,
+                    const=DEFAULT_MAX_ITER,
+                    metavar="ITERATIONS",
+                    help="perform no more than the specified number of "
+                         "analysis iterations. Lower is faster, but "
+                         "potentially less precise. A negative value specifies "
+                         "no cap on the iteration count. No cap by default.")
+
+parser.add_argument("-T",
+                    "--bail_time",
+                    type=int,
+                    nargs="?",
+                    default=DEFAULT_BAILOUT,
+                    const=DEFAULT_BAILOUT,
+                    metavar="SECONDS",
+                    help="begin to terminate the analysis if it's looking to "
+                         "take more time than the specified number of seconds. "
+                         "Bailing out early may mean the analysis is not able "
+                         "to reach a fixed-point, so results may be less "
+                         "precise. A negative value means no cap on the "
+                         "running time. No cap by default.")
+
+parser.add_argument("-q",
+                    "--quiet",
+                    action="store_true",
+                    default=False,
+                    help="Silence output.")
+
+parser.add_argument("-s",
+                    "--strict",
+                    action="store_true",
+                    default=False,
+                    help="unrecognised opcodes will not be skipped, but will "
+                         "result in an error.")
 
 
 def analyse_contract(filename, result_queue):
   """Perform dataflow analysis on a contract, storing the result in the queue."""
   try:
-    with open(join(contract_dir, filename)) as file:
-      cfg = tac_cfg.TACGraph.from_bytecode(file, strict=False)
+    with open(join(args.contract_dir, filename)) as file:
+      cfg = tac_cfg.TACGraph.from_bytecode(file, strict=args.strict)
 
-      for _ in range(analysis_iterations):
-        dataflow.stack_analysis(cfg)
-        cfg.clone_ambiguous_jump_blocks()
-      cfg.hook_up_def_site_jumps()
-      dataflow.stack_analysis(cfg, generate_throws=True)
-
-      cfg.remove_unreachable_code()
-      cfg.merge_duplicate_blocks(ignore_preds=True, ignore_succs=True)
-      cfg.hook_up_def_site_jumps()
+      dataflow.analyse_graph(cfg, max_iterations=args.max_iter,
+                                  bailout_seconds=args.bail_time)
 
       if cfg.has_unresolved_jump:
-        result_queue.put((filename, unresolved))
+        result_queue.put((filename, UNRESOLVED))
         ll("Unresolved.")
       else:
-        result_queue.put((filename, resolved))
+        result_queue.put((filename, RESOLVED))
 
   except Exception as e:
     ll("Error: {}".format(e))
-    result_queue.put((filename, error))
+    result_queue.put((filename, ERROR))
 
 
 def flush_queue(period, run_signal, result_queue, result_dict):
@@ -83,86 +170,90 @@ def flush_queue(period, run_signal, result_queue, result_dict):
       result_dict[item[1]] = result_dict[item[1]] + [item[0]]
       # NB: += doesn't work here
 
-if __name__ == "__main__":
-  if "-q" in sys.argv:
-    logger.LOG_LEVEL = logger.Verbosity.SILENT
-  else:
-    logger.LOG_LEVEL = logger.Verbosity.MEDIUM
 
-  # Extract contract filenames.
-  runtime_files = filter(lambda filename: filename.endswith("runtime.hex"),
-                         listdir(contract_dir))
-  if from_file:
-    with open(filenames_file, 'r') as f:
-      runtime_files = [l.strip() for l in f.readlines()]
+args = parser.parse_args()
 
-  sliced = itertools.islice(runtime_files, start_index, stop_index)
-  if process_all:
-    sliced = runtime_files
+if args.quiet:
+  logger.LOG_LEVEL = logger.Verbosity.SILENT
+else:
+  logger.LOG_LEVEL = logger.Verbosity.MEDIUM
 
-  # Set up multiprocessing stuff.
-  manager = Manager()
-  res_dict = manager.dict()
-  res_queue = SimpleQueue()
+# Extract contract filenames.
+runtime_files = filter(lambda filename: filename.endswith("runtime.hex"),
+                       listdir(args.contract_dir))
+if args.from_file:
+  with open(args.from_file, 'r') as f:
+    runtime_files = [l.strip() for l in f.readlines()]
 
-  res_dict[resolved] = []
-  res_dict[unresolved] = []
-  res_dict[timeout] = []
-  res_dict[error] = []
+stop_index = None if args.num_contracts is None else args.skip + args.num_contracts
+to_process = itertools.islice(runtime_files, args.skip, stop_index)
 
-  run_signal = Event()
-  run_signal.set()
-  flush_proc = Process(target=flush_queue, args=(flush_period, run_signal,
-                                                 res_queue, res_dict))
-  flush_proc.start()
 
-  try:
-    # Process each contract in turn, timing out if it takes too long.
-    for i, fname in enumerate(sliced):
-      ll("{}: {}.".format(i, fname))
-      proc = Process(target=analyse_contract, args=(fname, res_queue))
+# Set up multiprocessing stuff.
+manager = Manager()
+res_dict = manager.dict()
+res_queue = SimpleQueue()
 
-      start_time = time.time()
-      proc.start()
+res_dict[RESOLVED] = []
+res_dict[UNRESOLVED] = []
+res_dict[TIMEOUT] = []
+res_dict[ERROR] = []
 
-      while time.time() - start_time < timeout_secs:
-        if proc.is_alive():
-          time.sleep(0.01)
-        else:
-          proc.join()
-          break
+run = Event()
+run.set()
+flush_proc = Process(target=flush_queue, args=(FLUSH_PERIOD, run,
+                                               res_queue, res_dict))
+flush_proc.start()
+
+try:
+  # Process each contract in turn, timing out if it takes too long.
+  for i, fname in enumerate(to_process):
+    ll("{}: {}.".format(i, fname))
+    proc = Process(target=analyse_contract, args=(fname, res_queue))
+
+    start_time = time.time()
+    proc.start()
+
+    while time.time() - start_time < args.timeout_secs:
+      if proc.is_alive():
+        time.sleep(0.01)
       else:
-        res_queue.put((fname, timeout))
-        proc.terminate()
-        ll("Timed out.")
+        proc.join()
+        break
+    else:
+      res_queue.put((fname, TIMEOUT))
+      proc.terminate()
+      ll("Timed out.")
 
-    # Conclude and write results to file.
-    ll("\nFinishing...\n")
-    run_signal.clear()
-    flush_proc.join(flush_period + 1)
+  # Conclude and write results to file.
+  ll("\nFinishing...\n")
+  run.clear()
+  flush_proc.join(FLUSH_PERIOD + 1)
 
-    makedirs(results_dir, exist_ok=True)
+  makedirs(args.results_dir, exist_ok=True)
 
-    r = res_dict[resolved]
-    u = res_dict[unresolved]
-    t = res_dict[timeout]
-    e = res_dict[error]
-    total = sum(len(l) for l in res_dict.values())
+  r = res_dict[RESOLVED]
+  u = res_dict[UNRESOLVED]
+  t = res_dict[TIMEOUT]
+  e = res_dict[ERROR]
+  total = sum(len(l) for l in res_dict.values())
 
-    with open(results_dir + "resolved.txt", 'w') as f:
-      f.write("\n".join(r) + "\n")
-    with open(results_dir + "unresolved.txt", 'w') as f:
-      f.write("\n".join(u) + "\n")
-    with open(results_dir + "timeout.txt", 'w') as f:
-      f.write("\n".join(t) + "\n")
-    with open(results_dir + "error.txt", 'w') as f:
-      f.writelines("\n".join(e) + "\n")
+  with open(join(args.results_dir, "resolved.txt"), 'w') as f:
+    f.write("\n".join(r) + "\n")
+  with open(join(args.results_dir, "unresolved.txt"), 'w') as f:
+    f.write("\n".join(u) + "\n")
+  with open(join(args.results_dir, "timeout.txt"), 'w') as f:
+    f.write("\n".join(t) + "\n")
+  with open(join(args.results_dir, "error.txt"), 'w') as f:
+    f.writelines("\n".join(e) + "\n")
 
-    ll("Resolved: {}/{}".format(len(r), total))
-    ll("Unresolved: {}/{}".format(len(u), total))
-    ll("Timed Out: {}/{}".format(len(t), total))
-    ll("Errors: {}/{}".format(len(e), total))
-  except Exception as e:
-    import traceback
-    traceback.print_exc()
-    flush_proc.terminate()
+  ll("Resolved: {}/{}".format(len(r), total))
+  ll("Unresolved: {}/{}".format(len(u), total))
+  ll("Timed Out: {}/{}".format(len(t), total))
+  ll("Errors: {}/{}".format(len(e), total))
+except Exception as e:
+  import traceback
+  traceback.print_exc()
+  flush_proc.terminate()
+
+
