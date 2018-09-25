fc0cef9788ae37410ec7576274df07080d8e65d6
nevillegrech <nevillegrech@gmail.com>
2018-09-25 11:29:41 +0300

    update backup scripts

--- logic/backup_results.sh
+++ logic/backup_results.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-tar -hzcvf contract_data.tar.gz &&
-rm -rf .temp &
-scp contract_data.tar.gz contract-library:/srv/ &&
+echo "compressing files"
+tar -hzcf contract_data.tar.gz .temp --exclude='*.facts' &&
+#rm -rf .temp &
+echo "copying archive to remote server"    
+scp contract_data.tar.gz contract-library:/srv/contract_data.tar.gz &&
+echo "extracting archive on remote server"
 ssh contract-library "cd /srv ; tar -xzf contract_data.tar.gz"
