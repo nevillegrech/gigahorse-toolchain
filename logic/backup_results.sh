5e0b1386442c9115b29518ac65eb7cd4cc28addc
Neville Grech <nevillegrech@gmail.com>
2018-09-25 20:58:09 +0200

    many bugfixes, introduce fresh statements

--- logic/backup_results.sh
+++ logic/backup_results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "compressing files"
-tar -hzcf contract_data.tar.gz .temp --exclude='*.facts' &&
+tar -hzcf contract_data.tar.gz .temp --exclude='*.facts'
 #rm -rf .temp &
 echo "copying archive to remote server"    
-scp contract_data.tar.gz contract-library:/srv/contract_data.tar.gz &&
+scp contract_data.tar.gz contract-library:/srv/contract_data/contract_data.tar.gz &&
 echo "extracting archive on remote server"
-ssh contract-library "cd /srv ; tar -xzf contract_data.tar.gz"
+ssh contract-library "cd /srv/contract_data ; nohup tar -xzf contract_data.tar.gz"
