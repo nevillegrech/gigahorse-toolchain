a720193982eaa6d9726eb130a107f99c66a88905
Neville Grech <nevillegrech@gmail.com>
2018-09-30 14:37:58 +0200

    simplify default solidity global function rendering

--- logic/backup_results.sh
+++ logic/backup_results.sh
@@ -5,4 +5,5 @@ tar -hzcf contract_data.tar.gz --exclude='*.facts' --exclude='*.csv' --exclude '
 echo "copying archive to remote server"    
 scp contract_data.tar.gz contract-library:/srv/contract_data/contract_data.tar.gz &&
 echo "extracting archive on remote server"
+ssh contract-library "cd /srv/contract_data ; mv .temp .temp.old"
 ssh contract-library "cd /srv/contract_data ; nohup tar -xzf contract_data.tar.gz"
