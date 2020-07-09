4d94ad93affae548076e2646eec2864ceadc9502
Yannis Smaragdakis <yannis@smaragd.org>
2020-07-09 10:57:39 +0300

    update source-decompile.sh

--- logic/source-decompile.sh
+++ logic/source-decompile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x
-../bin/generatefacts $1 facts-tmp
+\rm -rf facts-tmp
+../generatefacts $1 facts-tmp
 \rm -rf tac-facts-tmp
 mkdir tac-facts-tmp
 pushd .
 cd tac-facts-tmp
 souffle -F ../facts-tmp ../decompiler.dl
 souffle ../../../gigahorse-clients/source_decompiler.dl
-python3 ../../../gigahorse-clients/get_source.py -d -v
+python3.8 ../../../gigahorse-clients/get_source.py -v -d
 popd
