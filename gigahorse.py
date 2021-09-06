a206429546edaae5f2c606fdd0e7664bf0c97f23
Ilias Tsatiris <eliastsatiris@gmail.com>
2021-09-06 16:06:51 +0300

    Reimpl memory limit

--- gigahorse.py
+++ gigahorse.py
@@ -7,6 +7,7 @@ import itertools
 import json
 import logging
 import signal
+import resource
 import shutil
 import re
 import subprocess
@@ -66,6 +67,9 @@ DEFAULT_NUM_JOBS = max(int(cpu_count() * 0.9), 1)
 """Bugfix for one core systems."""
 
 """The number of subprocesses to run at once."""
+
+DEFAULT_MEMORY_LIMIT = 48 * 1_000_000_000
+"""Default memory limit for analyses processes (48 GB)"""
 
 # Command Line Arguments
 
@@ -499,6 +503,9 @@ def get_gigahorse_analytics(out_dir, analytics):
             else:
                 analytics[stat_name] = ''
 
+def set_memory_limit():
+    resource.setrlimit(resource.RLIMIT_AS, (DEFAULT_MEMORY_LIMIT, DEFAULT_MEMORY_LIMIT))
+
 def run_process(args, timeout: int, stdout=devnull, stderr=devnull, cwd: str='.') -> float:
     ''' Runs process described by args, for a specific time period
     as specified by the timeout.
@@ -512,7 +519,7 @@ def run_process(args, timeout: int, stdout=devnull, stderr=devnull, cwd: str='.'
     start_time = time.time()
 
     try:
-        subprocess.run(args, timeout=timeout, stdout=stdout, stderr=stderr, cwd=cwd, env=souffle_env)
+        subprocess.run(args, timeout=timeout, stdout=stdout, stderr=stderr, cwd=cwd, env=souffle_env, preexec_fn=set_memory_limit)
     except subprocess.TimeoutExpired:
         return -1
 
