f482893f492443064c4cfec844c940bbdce62967
Sifis Lagouvardos <sifislag@gmail.com>
2022-10-17 15:43:45 +0300

    Initial support for error sigs

--- src/common.py
+++ src/common.py
@@ -3,4 +3,5 @@ import os
 
 public_function_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'PublicFunctionSignature.facts')
 event_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'EventSignature.facts')
+error_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'ErrorSignature.facts')
 
