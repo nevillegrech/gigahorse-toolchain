fc841af915ce37b1496110780439672c67629338
Sifis Lagouvardos <sifislag@gmail.com>
2022-11-16 18:07:05 +0200

    Added test for indexing a calldata array.

--- tests/memory-modeling/arrays/Arrays.sol
+++ tests/memory-modeling/arrays/Arrays.sol
@@ -1,6 +1,6 @@
 pragma solidity >= 0.8.0;
 
-contract CallDataArrays {
+contract CallDataToMemoryArrays {
 
   function get(address[] memory addr) external pure returns (address[] memory) {
       return addr;
@@ -14,4 +14,19 @@ contract CallDataArrays {
       return bools;
   }
 
+}
+
+contract CallDataArrayGet {
+
+  function _get(address[] calldata addr, uint256 index) private pure returns (address) {
+      return addr[index];
+  }
+
+  function get(address[] calldata addr, uint256 index) external pure returns (address) {
+      return _get(addr, index);
+  }
+
+  function get(address[] calldata addr, uint256 index1, uint256 index2) external pure returns (address, address) {
+      return (_get(addr, index1), _get(addr, index2));
+  }
 }
