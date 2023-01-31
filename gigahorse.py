de6d3ef1650b35825eb0fcbfcd217da780d60da4
Sifis Lagouvardos <sifislag@gmail.com>
2023-01-31 13:00:04 +0200

    Enabled DEBUG_OUTPUTs when using the --debug flag.

--- gigahorse.py
+++ gigahorse.py
@@ -285,13 +285,16 @@ def prepare_working_dir(contract_name) -> (bool, str, str, str):
     return False, newdir, out_dir, fallback_out_dir
 
 def get_souffle_macros():
-    souffle_macros = f'GIGAHORSE_DIR={GIGAHORSE_DIR} BULK_ANALYSIS= {args.souffle_macros}'.strip()
+    souffle_macros = f'GIGAHORSE_DIR={GIGAHORSE_DIR} {args.souffle_macros}'.strip()
+
+    if not args.debug:
+        souffle_macros += ' BULK_ANALYSIS='
 
     if args.enable_limitsize:
-        souffle_macros+=' ENABLE_LIMITSIZE='
+        souffle_macros += ' ENABLE_LIMITSIZE='
 
     if args.early_cloning:
-        souffle_macros+=' BLOCK_CLONING=HeuristicBlockCloner'
+        souffle_macros += ' BLOCK_CLONING=HeuristicBlockCloner'
 
     return souffle_macros
 
