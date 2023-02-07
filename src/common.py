35e5b9fc6492e701e4798cb483feea9e7a9dbfb2
Sifis Lagouvardos <sifislag@gmail.com>
2023-02-07 15:22:48 +0200

    update sig file location logic

--- src/common.py
+++ src/common.py
@@ -1,7 +1,11 @@
-from os.path import abspath, dirname, join
-import os
+from os.path import abspath, dirname, join, exists
 
-public_function_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'PublicFunctionSignature.facts')
-event_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'EventSignature.facts')
-error_signature_filename = join(join(dirname(abspath(__file__)), '..'), 'ErrorSignature.facts')
+def __get_sig_file(simple_filename):
+    preferred_dest = join(join(dirname(abspath(__file__)), '../../common-facts'), simple_filename)
+    fallback_dest = join(join(dirname(abspath(__file__)), '..'), simple_filename)
+    return preferred_dest if exists(preferred_dest) else fallback_dest
+
+public_function_signature_filename = __get_sig_file('PublicFunctionSignature.facts')
+event_signature_filename = __get_sig_file('EventSignature.facts')
+error_signature_filename = __get_sig_file('ErrorSignature.facts')
 
