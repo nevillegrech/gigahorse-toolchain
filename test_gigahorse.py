cf4fe8459560d2ffcbc315d73a2907df8ad625eb
Sifis Lagouvardos <sifislag@gmail.com>
2023-04-12 18:00:23 +0300

    refactor to use pytest, not parametrizable

--- test_gigahorse.py
+++ test_gigahorse.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse
 import subprocess
-import unittest
+import pytest
 import json
-import re
 from os.path import abspath, dirname, join, isdir, isfile
 from os import listdir, makedirs
 from typing import Mapping, MutableMapping, Any, List, Iterator, Tuple
@@ -16,7 +14,7 @@ DEFAULT_TEST_DIR = join(GIGAHORSE_TOOLCHAIN_ROOT, 'tests')
 TEST_WORKING_DIR = join(GIGAHORSE_TOOLCHAIN_ROOT, '.tests')
 
 
-class LogicTestCase(unittest.TestCase):
+class LogicTestCase():
     def __init__(self, name: str, test_root:str, test_path: str, test_config: Mapping[str, Any]):
         super(LogicTestCase, self).__init__()
 
@@ -73,10 +71,9 @@ class LogicTestCase(unittest.TestCase):
         with open(path) as f:
             return len(f.readlines())
 
-    def runTest(self):
+    def run(self):
         def within_margin(actual: int, expected: int, margin: float) -> bool:
             return (1 - margin) * expected <= actual <= (1 + margin) * expected
-
         result = self.__run()
 
         with open(join(self.working_dir, 'stdout'), 'wb') as f:
@@ -85,7 +82,7 @@ class LogicTestCase(unittest.TestCase):
         with open(join(self.working_dir, 'stderr'), 'wb') as f:
             f.write(result.stderr)
 
-        self.assertTrue(result.returncode == 0, f"Gigahorse exited with an error code: {result.returncode}")
+        assert result.returncode == 0, f"Gigahorse exited with an error code: {result.returncode}"
 
         with open(self.results_file) as f:
             (_, _, _, temp_analytics), = json.load(f)
@@ -95,10 +92,10 @@ class LogicTestCase(unittest.TestCase):
             analytics[x] = y
 
         for metric, expected, margin in self.expected_analytics:
-            self.assertTrue(within_margin(analytics[metric], expected, margin), f"Value for {metric} ({analytics[metric]}) not within margin of expected value ({expected}).")
+            assert within_margin(analytics[metric], expected, margin), f"Value for {metric} ({analytics[metric]}) not within margin of expected value ({expected})."
 
         for metric, expected in self.expected_verbatim:
-            self.assertTrue(analytics[metric] == expected, f"Value for {metric} ({analytics[metric]}) not the expected value ({expected}).")
+            assert analytics[metric] == expected, f"Value for {metric} ({analytics[metric]}) not the expected value ({expected})."
 
 
 def discover_logic_tests(current_config: MutableMapping[str, Any], directory: str) -> Iterator[Tuple[Mapping[str, Any], str]]:
@@ -123,56 +120,24 @@ def discover_logic_tests(current_config: MutableMapping[str, Any], directory: st
             yield from discover_logic_tests(current_config, entry_path)
 
 
-def run_tests(test_dirs: List[str]):
+def collect_tests(test_dirs: List[str]):
     makedirs(TEST_WORKING_DIR, exist_ok=True)
 
     for test_dir in (abspath(x) for x in test_dirs):
         print(f'Running testcases under {test_dir}')
 
-        test_suite = unittest.TestSuite()
         for config, hex_path in discover_logic_tests({}, test_dir):
             test_id = hex_path[len(test_dir) + 1:-4].replace('/', '.')
             if config:
-                test_suite.addTest(LogicTestCase(test_id, test_dir, hex_path, config))
-
-        unittest.TextTestRunner(verbosity=2).run(test_suite)
+                testdata.append(pytest.param(LogicTestCase(test_id, test_dir, hex_path, config), id=test_id))
 
 
-def main():
-    def check_arg_is_regex(val: str) -> str:
-        try:
-            re.compile(val)
-        except re.error:
-            raise argparse.ArgumentTypeError
 
-        return val
+testdata = []
 
-    parser = argparse.ArgumentParser(
-        description="Gigahorse unit test driver"
-    )
-
-    parser.add_argument(
-        'dirs',
-        metavar='D',
-        type=str,
-        nargs='*',
-        default=[DEFAULT_TEST_DIR],
-        help='Test directories'
-    )
-
-    parser.add_argument(
-        "-p",
-        "--pattern",
-        default=".*",
-        metavar="REGEX",
-        type=check_arg_is_regex,
-        help="A regular expression. Used to choose specific unit tests"
-    )
-
-    args = parser.parse_args()
-
-    run_tests(args.dirs)
+collect_tests([DEFAULT_TEST_DIR])
 
 
-if __name__ == "__main__":
-    main()
+@pytest.mark.parametrize("gigahorse_test", testdata)
+def test_gigahorse(gigahorse_test):
+    gigahorse_test.run()
